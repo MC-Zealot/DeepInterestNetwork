ac74b1b3c2c8661c201502ce2378e0d351267403
MC-Zealot <263097241@qq.com>
2020-06-03 01:16:45 +0800

    deep fm

--- deepFM/Test.py
+++ deepFM/Test.py
@@ -5,21 +5,44 @@ import random
 import numpy as np
 import tensorflow as tf
 import sys
-from input import DataInput, DataInputTest
-from model import Model
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+from pandas.core.frame import DataFrame
+
+# os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 random.seed(1234)
 np.random.seed(1234)
-tf.set_random_seed(1234)
+tf.compat.v1.set_random_seed(1234)
 
 train_batch_size = 32
 test_batch_size = 512
-a="123"
+a = "123"
 
-with open('../din/dataset.pkl','rb') as f:
-    train_set = pickle.load(f)
-    test_set = pickle.load(f)
-    cate_list = pickle.load(f)
-    user_count, item_count, cate_count = pickle.load(f)
+with open('../din/dataset.pkl', 'rb') as f:
+  train_set = pickle.load(f)
+  train_set_mini = train_set[0:10000]
+  test_set = pickle.load(f)
+  test_set_mini = train_set[0:5000]
+  cate_list = pickle.load(f)
+  user_count, item_count, cate_count = pickle.load(f)
+
+
+with open('train_set_mini.pkl', 'wb') as outfile1:
+  pickle.dump(train_set_mini, outfile1)
+
+with open('test_set_mini.pkl', 'wb') as outfile2:
+  pickle.dump(test_set_mini, outfile2)
+# with open(str('train_mini.pkl'), 'rb') as f:
+#     train_set = pickle.load(f)
+# word = pickle.load(open("train_mini.pkl", 'r'), encoding='utf-8')
+with open('train_mini.pkl', 'rb') as f3:
+    data_dict = pickle.load(f3)
+# with open('test_mini.pkl', 'rb') as f1:
+#     test_set = pickle.load(f1)
+#
+# with open('cate.pkl', 'rb') as f2:
+#     cate_list = pickle.load(f2)
+#
+# c = DataFrame(cate_list)
+# cate_count = len(c.drop_duplicates())
+# user_count, item_count, cate_count = pickle.load(f)
 
