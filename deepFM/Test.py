bf8695b0e07e3840791197c773bec4a7e69caeef
taoyizhou <taoyizhou@meituan.com>
2020-06-04 11:10:52 +0800

    data

--- deepFM/Test.py
+++ deepFM/Test.py
@@ -48,7 +48,9 @@ def read_csv():
 
 
 train_set_mini, test_set_mini, cate_list = read_csv()
-
+user_count = len(train_set_mini['user_id'].drop_duplicates())
+viewed_item_id = len(train_set_mini['viewed_item_id'].drop_duplicates())
+cate_count = len(train_set_mini['cate_list'].drop_duplicates())
 # with open('train_set_mini.pkl', 'w') as outfile1:
 #   pickle.dump(train_set_mini, outfile1,0)
 #
