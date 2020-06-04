f6d7c836baa2e0ca10574341f92e1cc3959ced55
taoyizhou <taoyizhou@meituan.com>
2020-06-04 10:38:16 +0800

    pyspark lr

--- deepFM/Test.py
+++ deepFM/Test.py
@@ -16,27 +16,29 @@ tf.compat.v1.set_random_seed(1234)
 train_batch_size = 32
 test_batch_size = 512
 a = "123"
+def process_data():
+  with open('../din/dataset.pkl', 'rb') as f:
+    train_set = pickle.load(f)
+    train_set_mini = train_set[0:10000]
+    test_set = pickle.load(f)
+    test_set_mini = train_set[0:5000]
+    cate_list = pickle.load(f)
+    user_count, item_count, cate_count = pickle.load(f)
 
-with open('../din/dataset.pkl', 'rb') as f:
-  train_set = pickle.load(f)
-  train_set_mini = train_set[0:10000]
-  test_set = pickle.load(f)
-  test_set_mini = train_set[0:5000]
-  cate_list = pickle.load(f)
-  user_count, item_count, cate_count = pickle.load(f)
+    train_set_mini_df = DataFrame(train_set_mini)
+    train_set_mini_df.to_csv('train_set_mini.csv')
 
-  train_set_mini_df = DataFrame(train_set_mini)
-  train_set_mini_df.to_csv('train_set_mini.csv')
+    train_set_df = DataFrame(train_set)
+    train_set_df.to_csv('train_set.csv')
 
-  train_set_df = DataFrame(train_set)
-  train_set_df.to_csv('train_set.csv')
+    test_set_df = DataFrame(test_set)
+    test_set_df.to_csv('test_set.csv')
 
-  test_set_df = DataFrame(test_set)
-  test_set_df.to_csv('test_set.csv')
+    test_set_mini_df = DataFrame(test_set_mini)
+    test_set_mini_df.to_csv('test_set_mini.csv')
 
-  test_set_mini_df = DataFrame(test_set_mini)
-  test_set_mini_df.to_csv('test_set_mini.csv')
-
+    cate_list_df = DataFrame(cate_list)
+    cate_list_df.to_csv('cate_list.csv')
 # with open('train_set_mini.pkl', 'w') as outfile1:
 #   pickle.dump(train_set_mini, outfile1,0)
 #
@@ -47,8 +49,10 @@ with open('../din/dataset.pkl', 'rb') as f:
 # with open(str('train_mini.pkl'), 'rb') as f:
 #     train_set = pickle.load(f)
 # word = pickle.load(open("train_mini.pkl", 'r'), encoding='utf-8')
-with open('train_mini.pkl', 'r') as f3:
-    data_dict = pickle.load(f3)
+
+# with open('train_mini.pkl', 'r') as f3:
+#     data_dict = pickle.load(f3)
+
 # with open('test_mini.pkl', 'rb') as f1:
 #     test_set = pickle.load(f1)
 #
